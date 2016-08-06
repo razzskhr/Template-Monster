40298cebd010a6cb3949773bac155fe9678ab224
raj sekhar <razz.skhr008@gmail.com>
2016-08-06 11:02:41 +0530

    files changed

--- public/js/flipanimate.js
+++ public/js/flipanimate.js
@@ -1,5 +1,177 @@
 $(document).ready(function(){
 
+	$(".projects4Link").click(function(){
+
+		$(".prj4Imgs").css("display","none");
+
+		var dataIndex = $(this).attr("data-index");
+
+		if(dataIndex == 0)
+		{
+			location.reload();
+			
+			// $(".Category1").css("display","block");
+			// $(".Category2").css("display","block");
+			// $(".Category3").css("display","block");
+		}
+
+		if(dataIndex == 1)
+		{
+			var items = [] ;
+
+			$(".Category1").each(function(){
+				items.push($(this).html());
+			});
+
+			if(items.length <= 4)
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= items.length && items.length > 4)	
+			{
+				for (var i = 0; i < 4; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= 12 && items.length > 8)	
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+
+				for (var i = 8; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent3").append("<div class='col-sm-3 " + "prj4Imgs" + " Category1' >" + items[i] + "</div>");
+				}
+			}
+			
+		}
+		else if (dataIndex == 2) 
+		{
+			var items = [] ;
+
+			$(".Category2").each(function(){
+				items.push($(this).html());
+			});
+				
+			if(items.length <= 4)
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= 8 && items.length > 4)	
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= 12 && items.length > 8)	
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+
+				for (var i = 8; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent3").append("<div class='col-sm-3 " + "prj4Imgs" + " Category2' >" + items[i] + "</div>");
+				}
+			}
+		}
+		else if (dataIndex == 3) 
+		{
+			var items = [] ;
+
+			$(".Category3").each(function(){
+				items.push($(this).html());
+			});
+				
+			if(items.length <= 4)
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= 8 && items.length > 4)	
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+			}
+			else if(items.length <= 12 && items.length > 8)	
+			{
+				for (var i = 0; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent1").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+
+				for (var i = 4; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent2").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+
+				for (var i = 8; i < items.length; i++) 
+				{
+					// alert(items[i]);
+					$("#whatWeDoContent3").append("<div class='col-sm-3 " + "prj4Imgs" + " Category3' >" + items[i] + "</div>");
+				}
+			}
+		}
+
+	});
+
     $(".hoverOn").mouseenter(function(){
     	$(this).find("img").addClass("animated flip");
     });
